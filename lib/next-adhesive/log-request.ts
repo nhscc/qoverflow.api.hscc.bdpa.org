4d6936a7c43cfc18697f49db9301f3de0f771401
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-07-13 20:44:49 -0700

    fix(lib): properly pass endpoint descriptor metadata to request logging function

--- lib/next-adhesive/log-request.ts
+++ lib/next-adhesive/log-request.ts
@@ -2,6 +2,7 @@ import { addToRequestLog } from 'multiverse/next-log';
 import { debugFactory } from 'multiverse/debug-extended';
 
 import type { NextApiRequest, NextApiResponse } from 'next';
+import type { MiddlewareContext } from 'multiverse/next-api-glue';
 
 const debug = debugFactory('next-adhesive:log-request');
 
@@ -12,7 +13,11 @@ export type Options = {
 /**
  * Logs the response to each request after it is sent (i.e. `res.end()`).
  */
-export default async function (req: NextApiRequest, res: NextApiResponse) {
+export default async function (
+  req: NextApiRequest,
+  res: NextApiResponse,
+  context: MiddlewareContext<Options>
+) {
   debug('entered middleware runtime');
 
   const send = res.end;
@@ -23,7 +28,11 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     if (!sent) {
       debug('logging request after initial call to res.end');
       // ! Note that this async function is NOT awaited!!!
-      void addToRequestLog({ req, res, endpoint: '/fake' });
+      void addToRequestLog({
+        req,
+        res,
+        endpoint: context.runtime.endpoint.descriptor
+      });
     }
   }) as typeof res.end;
 }
