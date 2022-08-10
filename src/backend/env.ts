1d115a2e50ad5264be6959d90695d49a54f6ac6c
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2022-08-09 21:59:30 -0700

    refactor(src): add support for latest env variables

--- src/backend/env.ts
+++ src/backend/env.ts
@@ -10,6 +10,20 @@ import type { Environment } from 'multiverse/next-env';
 // eslint-disable-next-line @typescript-eslint/ban-types
 export function getEnv<T extends Environment = Environment>() {
   const env = getDefaultEnv({
+    STACKAPPS_INTERVAL_PERIOD_MS: Number(
+      process.env.STACKAPPS_INTERVAL_PERIOD_MS || 0
+    ),
+    STACKAPPS_MAX_REQUESTS_PER_INTERVAL:
+      Number(process.env.STACKAPPS_MAX_REQUESTS_PER_INTERVAL) || null,
+    STACKAPPS_TOTAL_API_GENERATED_QUESTIONS:
+      Number(process.env.STACKAPPS_TOTAL_API_GENERATED_QUESTIONS) || null,
+    STACKAPPS_COLLECTALL_QUESTION_ANSWERS:
+      Number(process.env.STACKAPPS_COLLECTALL_QUESTION_ANSWERS) || null,
+    STACKAPPS_COLLECTALL_QUESTION_COMMENTS:
+      Number(process.env.STACKAPPS_COLLECTALL_QUESTION_COMMENTS) || null,
+    STACKAPPS_COLLECTALL_FIRST_ANSWER_COMMENTS:
+      Number(process.env.STACKAPPS_COLLECTALL_FIRST_ANSWER_COMMENTS) || null,
+    STACKAPPS_MAX_PAGE_SIZE: Number(process.env.STACKAPPS_MAX_PAGE_SIZE) || null,
     STACKAPPS_AUTH_KEY: process.env.STACKAPPS_AUTH_KEY || null,
     MAX_PARAMS_PER_REQUEST: Number(process.env.MAX_PARAMS_PER_REQUEST) || 100,
     MIN_USER_NAME_LENGTH: Number(process.env.MIN_USER_NAME_LENGTH) || 4,
