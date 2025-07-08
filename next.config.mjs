d09725b86eca2c6cd08a0af3593e90fbdab1b28a
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-07-07 17:33:59 -0700

    build(next): improve DX

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@
 export default function nextConfig() {
   return {
     // * https://nextjs.org/docs/app/api-reference/config/next-config-js/allowedDevOrigins
-    allowedDevOrigins: ['*.romulus'],
+    allowedDevOrigins: ['*.romulus', 'qoverflow.api.hscc.bdpa.romulus'],
 
     // ? Default is explicitly provided for the benefit of tooling
     distDir: '.next',
